6338f8de3c4ee0bddea6326effda04d58bd4c14e
Aiden <aiden@adsl-172-10-0-4.dsl.sndg02.sbcglobal.net>
2022-04-20 22:20:01 +0700

    fix st

--- mongorely/mongomand_create.go
+++ mongorely/mongomand_create.go
@@ -8,12 +8,18 @@ import (
 
 func Create(ctx context.Context, model MongoModel, opts ...*options.InsertOneOptions) (interface{}, error) {
 	col := db.Collection(model.GetMongoCollName())
-	result, err := col.InsertOne(ctx, model, opts...)
-	return result.InsertedID, err
+	if result, err := col.InsertOne(ctx, model, opts...); err != nil {
+		return nil, err
+	} else {
+		return result.InsertedID, nil
+	}
 }
 
 func CreateCustom(ctx context.Context, collectionName string, model interface{}, opts ...*options.InsertOneOptions) (interface{}, error) {
 	col := db.Collection(collectionName)
-	result, err := col.InsertOne(ctx, model, opts...)
-	return result.InsertedID, err
+	if result, err := col.InsertOne(ctx, model, opts...); err != nil {
+		return nil, err
+	} else {
+		return result.InsertedID, err
+	}
 }
