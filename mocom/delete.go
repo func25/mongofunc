aeba43a8ab1abe9d0bd63cf18538b51f815cda76
phuong <phuong@wolffun>
2022-06-27 23:34:53 +0700

    update deleteOne

---
+++ mocom/delete.go
@@ -0,0 +1,13 @@
+package mocom
+
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func DeleteOne[T Model](ctx context.Context, filter interface{}, opts ...*options.DeleteOptions) (*mongo.DeleteResult, error) {
+	var t T
+	return collWrite(t.CollName()).DeleteOne(ctx, filter, opts...)
+}
