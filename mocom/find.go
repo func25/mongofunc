e70e47e22c4090cb9370a1c6816ba9a8f4322e5d
phuong <phuong@wolffun>
2023-10-24 20:29:00 +0700

    update generic name

--- mocom/find.go
+++ mocom/find.go
@@ -6,9 +6,9 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-// Find finds documents from from model's collection
+// FindT finds documents from from model's collection
 // model should be implement `CollName() string`
-func Find[T Modeler](ctx context.Context, filter interface{}, opts ...*options.FindOptions) (res []T, err error) {
+func FindT[T Modeler](ctx context.Context, filter interface{}, opts ...*options.FindOptions) (res []T, err error) {
 	var t T
 	cur, err := CollRead(t.CollName()).Find(ctx, filter, opts...)
 	if err != nil {
@@ -19,9 +19,9 @@ func Find[T Modeler](ctx context.Context, filter interface{}, opts ...*options.F
 	return res, err
 }
 
-// FindOne finds one document from model's collection
+// FindOneT finds one document from model's collection
 // model should be implement `CollName() string`
-func FindOne[T Modeler](ctx context.Context, filter interface{}, opts ...*options.FindOneOptions) (res *T, err error) {
+func FindOneT[T Modeler](ctx context.Context, filter interface{}, opts ...*options.FindOneOptions) (res *T, err error) {
 	res = new(T)
 	var t T
 	cur := CollRead(t.CollName()).FindOne(ctx, filter, opts...)
